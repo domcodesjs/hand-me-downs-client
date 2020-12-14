1893cd90f69f180e37e615a2765e83fec964ce2c
Dominic Faulring <domcodesjs@gmail.com>
2020-12-13 19:04:03 -0800

    added how to video

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,15 @@ const Home = () => {
   return (
     <>
       <SearchForm></SearchForm>
+      <iframe
+        width='100%'
+        height='400px'
+        title='how to use hand me downs'
+        src='https://www.youtube.com/embed/OQu32o0MFpQ'
+        frameborder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowfullscreen
+      ></iframe>
       <LatestListings></LatestListings>
     </>
   );
