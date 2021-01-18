0ca885a4b368c016da458f4c46f4d29ea0767c21
Dominic Faulring <domcodesjs@gmail.com>
2021-01-18 12:44:55 -0800

    fixed iframe warnings

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,9 +11,9 @@ const Home = () => {
         height='400px'
         title='how to use hand me downs'
         src='https://www.youtube.com/embed/OQu32o0MFpQ'
-        frameborder='0'
+        frameBorder='0'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowfullscreen
+        allowFullScreen
       ></iframe>
       <LatestListings></LatestListings>
     </>
